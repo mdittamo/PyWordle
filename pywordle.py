a289e185bd85f561347d59c6eeb8aa28c611a88c
mdittamo <mike.dittamo@gmail.com>
2022-11-22 16:11:13 -0500

    Update pywordle.py

--- pywordle.py
+++ pywordle.py
@@ -1,10 +1,9 @@
 import getpass
+import pyfiglet
 import random
 from colorama import Fore, Back, Style
 from colorama import init
 init()
-
-alphabet = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0, 'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}
 
 #Obtain word from user(1)
 def getword():
@@ -27,7 +26,9 @@ def getguess():
         if len(guess) < 5 or len(guess) > 5 or all(chr.isalpha() for chr in guess) == False: 
             print(Fore.RED + '\nNot the correct number or invalid characters!' + Style.RESET_ALL)
     return list(guess)
-    
+
+logo = pyfiglet.figlet_format("PyWordle" )
+print(Fore.GREEN + logo + Style.RESET_ALL)    
     
 #Player Class
 class Player:
@@ -36,7 +37,8 @@ class Player:
         
 gameon = True  
 
-while gameon == True:     
+while gameon == True:    
+    
     #Game Logic
     #Initialize game variables
     correct_guess = False
@@ -44,7 +46,6 @@ while gameon == True:
     results = ['_', '_','_','_','_']
 
     #Title Screen
-    print(Fore.GREEN + '\nWelcome to PyWordle!\n' + Style.RESET_ALL)
     print(f'Guess the word in {player1.score} tries.')
     
     #Player supplied word
@@ -54,7 +55,8 @@ while gameon == True:
     word = random.choice(open('words.txt').readlines())
     word = word.upper()
     word = word[0:5]
-    #Update alpha dictionary
+    #Update alpha dictionary - reset game
+    alphabet = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0, 'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}
     for x in range(len(word)): 
         alphabet[word[x]]+=1
     #print(word)
@@ -96,6 +98,7 @@ while gameon == True:
                     results[x] = 'X'
                     alphabet[guess[x]]-=1
             
+            
             #Check for O's - Correct letter wrong place
             for x in range (len(guess)): 
                 for y in range(len(l_word)):
@@ -103,10 +106,10 @@ while gameon == True:
                         results[x] = 'O'
                         alphabet[guess[x]]-=1
                         
-            print('\n' + str(results))
-            print(f'\nYou have {player1.score} guesses left.')
-            results = ['_', '_','_','_','_']
-            
             alphabet = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0, 'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}
             for x in range(len(word)): 
                 alphabet[word[x]]+=1
+                        
+            print('\n' + str(results))
+            print(f'\nYou have {player1.score} guesses left.')
+            results = ['_', '_','_','_','_']
