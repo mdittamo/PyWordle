a4e85b1640c5ff70dbe92279a2cc9f670a8425d9
mdittamo <mike.dittamo@gmail.com>
2022-11-22 08:24:51 -0500

    Update pywordle.py

--- pywordle.py
+++ pywordle.py
@@ -31,44 +31,60 @@ class Player:
     def __init__ (self, score):
         self.score = score
         
-#Game Logic
-#Initialize game variables
-correct_guess = False
-player1 = Player(5)
-results = ['_', '_','_','_','_']
+gameon = True  
 
-#Title Screen
-print(Fore.GREEN + '\nWelcome to PyWordle!\n' + Style.RESET_ALL)
-print(f'Guess the word in {player1.score} tries.\n')
-word = getword()
+while gameon == True:     
+    #Game Logic
+    #Initialize game variables
+    correct_guess = False
+    player1 = Player(5)
+    results = ['_', '_','_','_','_']
 
-#Gameplay
-while correct_guess == False: 
-    guess = getguess()
-    
-    if guess == word: 
-        print(Fore.GREEN + '\nYou win!\nYou win!\nYou win!' + Style.RESET_ALL)
-        break
-    
-    elif player1.score <=1: 
-        go_word = ''.join(word)
-        print(Fore.RED + f'\nYou lose!\nThe word was {go_word}!\nYou lose!' + Style.RESET_ALL)
-        break
+    #Title Screen
+    print(Fore.GREEN + '\nWelcome to PyWordle!\n' + Style.RESET_ALL)
+    print(f'Guess the word in {player1.score} tries.\n')
+    word = getword()
+
+    #Gameplay
+    while correct_guess == False: 
+        guess = getguess()
         
-    else: 
-        player1.score-=1
+        if guess == word: 
+            print(Fore.GREEN + '\nYou win!\nYou win!\nYou win!\n' + Style.RESET_ALL)
+            choice = input('Would you like to play again (Y)? ')
+            choice = choice.upper()
+            if choice == 'Y':
+                break
+            else: 
+                gameon = False
+                break
         
-        #Check for O's - Correct letter wrong place
-        for x in range (len(guess)): 
-            for y in range(len(word)):
-                if guess[x] == word[y]:
-                    results[x] = 'O'
-        
-        #Check for X's - Correct letter correct place
-        for x in range (len(guess)): 
-            if guess[x] == word[x]:
-                results[x] = 'X'
-        
-        print('\n' + str(results))
-        print(f'\nYou have {player1.score} guesses left.')
-        results = ['_', '_','_','_','_']
+        elif player1.score <=1: 
+            go_word = ''.join(word)
+            print(Fore.RED + f'\nYou lose!\nThe word was {go_word}!\nYou lose!\n' + Style.RESET_ALL)
+            choice = input('Would you like to play again (Y)? ')
+            choice = choice.upper()
+            if choice == 'Y':
+                break
+            else: 
+                gameon = False
+                break
+            break
+            
+        else: 
+            player1.score-=1
+            
+            #Check for O's - Correct letter wrong place
+            for x in range (len(guess)): 
+                for y in range(len(word)):
+                    if guess[x] == word[y]:
+                        results[x] = 'O'
+            
+            #Check for X's - Correct letter correct place
+            for x in range (len(guess)): 
+                if guess[x] == word[x]:
+                    results[x] = 'X'
+            
+            print('\n' + str(results))
+            print(f'\nYou have {player1.score} guesses left.')
+            results = ['_', '_','_','_','_']
